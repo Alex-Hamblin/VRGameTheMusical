3a811e7b74d8e264b576d57344fd4e2cc0653b7e
VantasTheViking <72925846+VantasTheViking@users.noreply.github.com>
2024-11-02 02:57:19 -0400

    deagle

--- Assets/Scripts/Deagle.cs
+++ Assets/Scripts/Deagle.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Deagle : MonoBehaviour
 {
+
+    [SerializeField] InputActionProperty fireAction;
+    [SerializeField] InputActionProperty reloadAction;
+
+    [SerializeField] int bulletsLeft;
+    [SerializeField] int bulletsMax;
+
+    [SerializeField] AudioClip fireSFX;
+    [SerializeField] AudioClip emptyFireSFX;
+    [SerializeField] AudioClip reloadSFX;
+
+    [SerializeField] AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bulletsLeft = bulletsMax;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (fireAction.action.ReadValue<bool>())
+        {
+
+            fire();
+
+        }else 
         
+        if (reloadAction.action.ReadValue<bool>())
+        {
+            reload();
+
+        }
+    }
+
+
+    void reload()
+    {
+        bulletsLeft = bulletsMax;
+        PlayAudioClip(reloadSFX);
+
+    }
+
+    void fire()
+    {
+        if (bulletsLeft > 0)
+        {
+            bulletsLeft--;
+            PlayAudioClip(fireSFX);
+        } else
+        {
+            PlayAudioClip(emptyFireSFX);
+        }
+    }
+
+    void PlayAudioClip(AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
     }
 }
